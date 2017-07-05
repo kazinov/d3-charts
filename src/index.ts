0eea07147a758795cc2f5b829e8b48ece3fe39b3
kazinov <dm.kazinov@gmail.com>
2017-07-05 16:41:36 +0200

    animations

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,17 @@ import { LineChart } from './line-chart/line-chart.ts';
 
 const lineChart = new LineChart(document.getElementById('line-chart-container'));
 lineChart.updateData([4, 8, 15, 16, 23, 42]);
+
+
+const dataInput: HTMLInputElement = <HTMLInputElement>document.getElementById('data');
+dataInput.addEventListener('change', () => {
+    const numberPattern = /\d+/g;
+
+    let datum: number[] = [];
+    const numbers = dataInput.value.match(numberPattern);
+    if (numbers && numbers.length) {
+        datum = numbers.map((val: string) => + val);
+    }
+
+    lineChart.updateData(datum);
+});
--- src/line-chart/line-chart.ts
+++ src/line-chart/line-chart.ts
@@ -1,8 +1,9 @@
 import * as d3 from 'd3';
 import './line-chart.css';
+import { selection } from 'd3-selection';
 
 export class LineChart {
-    private svg = d3.select(this.container).append("svg").classed('line-chart', true);
+    private svg = d3.select(this.container).append("svg").attr("width", this.width).classed('line-chart', true);
 
     constructor(private container: Element, private width = 420, private barHeight = 20) {
     }
@@ -13,22 +14,39 @@ export class LineChart {
             .range([0, this.width]);
 
         var chart = this.svg
-            .attr("width", this.width)
             .attr("height", this.barHeight * data.length);
 
-        var bar = chart.selectAll("g")
-            .data(data)
-            .enter().append("g")
-            .attr("transform", (d, i) => { return "translate(0," + i * this.barHeight + ")"; });
+        const g = (selection: d3.Selection<any, any, any, any>) => {
+            selection.attr("transform", (d, i) => { return "translate(0," + i * this.barHeight + ")"; })
+        }
 
-        bar.append("rect")
-            .attr("width", x)
-            .attr("height", this.barHeight - 1);
+        const rect = (selection: d3.Selection<any, any, any, any>) => {
+            selection
+                .transition()
+                .duration(750)
+                .attr("width", x)
+                .attr("height", this.barHeight - 1);
+        }
 
-        bar.append("text")
-            .attr("x", (d) => { return x(d) - 3; })
-            .attr("y", this.barHeight / 2)
-            .attr("dy", ".35em")
-            .text((d) => { return d; });
+        const text = (selection: d3.Selection<any, any, any, any>) => {
+            selection
+                .attr("x", (d) => { return x(d) - 3; })
+                .attr("y", this.barHeight / 2)
+                .attr("dy", ".35em")
+                .text((d) => { return d; });
+        }
+
+
+        var bar = chart.selectAll("g").data(data);
+        g(bar.select("g"));
+        rect(bar.select("rect"));
+        text(bar.select("text"));
+
+        const barEnter = bar.enter().append("g");
+        g(barEnter);
+        rect(barEnter.append("rect"));
+        text(barEnter.append("text"));
+
+        bar.exit().remove();
     }
 }
